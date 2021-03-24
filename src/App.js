cab3124c07c68171262577776a46144c8e955338
Thanh V <thanh.vu@savvycomsoftware.com>
2021-03-24 13:48:33 +0700

    feat: create function scroll call api load page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import debounce from 'lodash.debounce';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.onscroll = debounce(() => {
+      if (window.innerHeight + document.documentElement.scrollTop === document.scrollingElement.scrollHeight) {
+        setPage(page + 1);
+      }
+      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) {
+        if (page <= 1) {
+          setPage(0)
+        } else {
+          setPage(page - 1);
+        }
+      }
+      // console.log(window.innerHeight, document.documentElement.scrollTop, document.documentElement.offsetHeight);
+      // console.log(window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight);
+    }, 1000);
+  }, [page]);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://quill-fog-route.glitch.me/dreams/' + page)
+      .then(res => res.json())
+      .then(res => {
+        setData(res);
+        setLoading(false);
+      })
+      .catch(err => err);
+  }, [page]);
+
+  const showText = () => {
+    return data.map((item, i) => <div key={i} style={{ height: '200px' }}>{item}</div>)
+  }
+
+  const showLoading = () => {
+    return loading && <div className="loading">
+      <div className="lds-ripple">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showLoading()}
+      {showText()}
     </div>
   );
 }
